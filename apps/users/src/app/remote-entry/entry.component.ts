3807864daf9c7104f84e14baba5c229b5891339c
Nicolas Potier Anzola <n.potier@uniandes.edu.co>
2025-03-09 13:23:08 -0500

    add members view

--- apps/users/src/app/remote-entry/entry.component.ts
+++ apps/users/src/app/remote-entry/entry.component.ts
@@ -1,9 +1,91 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { CustomDialogComponent } from '../components/custom-dialog/custom-dialog.component';
+
+type Member = {
+  name: string;
+  mail: string;
+};
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   selector: 'app-users-entry',
-  template: `<b>Users View</b>`,
+  styleUrl: './users-entry.component.scss',
+  templateUrl: './users-entry.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  public members: Member[] = [
+    {
+      name: 'Juan Perez',
+      mail: 'juan.perez@gmail.com',
+    },
+    {
+      name: 'Rosita Rosales',
+      mail: 'rosita.rosales@gmail.com',
+    },
+    {
+      name: 'Sofia Perez',
+      mail: 'sofia.perez@gmail.com',
+    },
+    {
+      name: 'Santiago Gonzalez',
+      mail: 'santiago.gonzalez@gmail.com',
+    },
+    {
+      name: 'Nicolás Perez',
+      mail: 'nicolas.perez@gmail.com',
+    },
+    {
+      name: 'Sancho Panza',
+      mail: 'sancho.panza@gmail.com',
+    },
+    {
+      name: 'Don Quijote',
+      mail: 'quijote@gmail.com',
+    },
+  ];
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CustomDialogComponent, {
+      data: {
+        title: 'Invitar Miembro',
+        message: 'Ingrese el correo del miembro a invitar al equipo',
+        input: true,
+        disabledButton: false,
+        cancelButton: true,
+        cancelLabel: 'Cerrar',
+        acceptLabel: 'Invitar',
+        inputLabel: 'juan.diego@gmail.com'
+      },
+      width: '580px',
+      minHeight: '174px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.dialog.open(CustomDialogComponent, {
+          data: {
+            title: 'Invitar Miembro',
+            message: 'Miembro Invitado con éxito',
+            input: false,
+            disabledButton: false,
+            cancelButton: false,
+            cancelLabel: 'Cerrar',
+            acceptLabel: 'Cerrar',
+            inputLabel: ''
+          },
+          width: '580px',
+          minHeight: '174px',
+        });
+      }
+    });
+  }
+}
